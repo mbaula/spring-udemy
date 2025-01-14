75ed14c4bcbd2c1177244451c1359226ddfa264c
mbaula <markbaula.1@gmail.com>
2025-01-14 12:13:01 -0500

    playing w/ spring - autowiring, reusing beans

--- learn-spring-framework/src/main/java/com/markb/learn_spring_framework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/markb/learn_spring_framework/App02HelloWorldSpring.java
@@ -17,6 +17,17 @@ public class App02HelloWorldSpring {
 		// 3. Retrieve beans managed by Spring
 		System.out.println(context.getBean("name"));
 		
+		System.out.println(context.getBean("age"));
+		
+		System.out.println(context.getBean("person"));
+		
+		System.out.println(context.getBean("person2MethodCall"));
+		
+		System.out.println(context.getBean("person3Parameters"));
+		
+		System.out.println(context.getBean("address2"));
+		
+		System.out.println(context.getBean(String.class));
 	}
 
 }
--- learn-spring-framework/src/main/java/com/markb/learn_spring_framework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/markb/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,11 +3,41 @@ package com.markb.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age, Address address) {};
+record Address (String firstLine, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 	
 	@Bean
 	public String name() {
 		return "Mark";
+	}
+	
+	@Bean
+	public int age() {
+		return 23;
+	}
+	
+	@Bean
+	public Person person() {
+		var person = new Person("Jack", 24, new Address("Main St", "Berlin"));
+		return person;
+	}
+	
+	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address());
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address address2) {
+		return new Person(name, age, address2);
+	}
+	
+	@Bean (name="address2")
+	public Address address() {
+		var address = new Address("1 University", "London");
+		return address;
 	}
 }
